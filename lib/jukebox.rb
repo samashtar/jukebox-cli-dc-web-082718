1555e7fb7f43a8dc464eaa13720d17a7dcbbd044
Sam Ashtar <samashtarm@gmail.com>
2018-08-25 05:47:48 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -33,8 +33,7 @@ end
 
 def play (songs)
   puts "Please enter a song name or number:"
- getuser = gets.chomp
-
+  user_response = gets.downcase.chomp 
   if  (1..9).to_a.include?(user_response.to_i)
     puts "Playing #{songs[user_response.to_i - 1]}"
     elsif songs.include?(user_response)
